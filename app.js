5050e347233f63ae0c1eeadcfdf63d4e5c01f656
Jason Allen <allenja2@flip1.engr.oregonstate.edu>
2021-11-17 08:51:47 -0800

    step 3 starter

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-
-// App.js
-
 /*
     SETUP
 */
+//Express
 var express = require('express');   // We are using the express library for the web server
 var app = express();            // We need to instantiate an express object to interact with the server in our code
 PORT = 7634;                 // Set a port number at the top so it's easy to change in the future
@@ -11,40 +9,30 @@ PORT = 7634;                 // Set a port number at the top so it's easy to cha
 // Database
 var db = require('./database/db-connector')
 
+//Express-Handlebars
+const { engine } = require('express-handlebars');
+const { query } = require('express');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({ extname: '.hbs', defaultLayout: 'main' }));
+
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+
 
 /*
     ROUTES
 */
-// app.js 
+// app.js
 
 app.get('/', function (req, res) {
-    // Define our queries
-    query1 = 'DROP TABLE IF EXISTS diagnostic;';
-    query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-    query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
-    query4 = 'SELECT * FROM diagnostic;';
+    let query1 = "SELECT * FROM DDL_SSS;";               // Define our query
 
-    // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+    db.pool.query(query1, function (error, rows, fields) {    // Execute the query
 
-    // DROP TABLE...
-    db.pool.query(query1, function (err, results, fields) {
+        res.render('index', { data: rows });                  // Render the index.hbs file, and also send the renderer
+    })                                                      // an object where 'data' is equal to the 'rows' we
+});                                                         // received back from the query
 
-        // CREATE TABLE...
-        db.pool.query(query2, function (err, results, fields) {
-
-            // INSERT INTO...
-            db.pool.query(query3, function (err, results, fields) {
-
-                // SELECT *...
-                db.pool.query(query4, function (err, results, fields) {
-
-                    // Send the results to the browser
-                    res.send(JSON.stringify(results));
-                });
-            });
-        });
-    });
-});
 
 /*
     LISTENER
